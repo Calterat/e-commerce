7178f60e105aed94e8bd26972171f2f8a7bcc876
Calterat <Calterat@gmail.com>
2021-01-12 22:59:17 -0600

    Kind of finished quicker than I thought... I don't think I saved once. The Pseudo code in this project helped a bunch

--- seeds/index.js
+++ seeds/index.js
@@ -6,8 +6,11 @@ const seedProductTags = require('./product-tag-seeds');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
+  
   await sequelize.sync({ force: true });
+
   console.log('\n----- DATABASE SYNCED -----\n');
+  
   await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
 
@@ -16,7 +19,7 @@ const seedAll = async () => {
 
   await seedTags();
   console.log('\n----- TAGS SEEDED -----\n');
-
+  
   await seedProductTags();
   console.log('\n----- PRODUCT TAGS SEEDED -----\n');
 
@@ -24,3 +27,27 @@ const seedAll = async () => {
 };
 
 seedAll();
+
+// This was code I used to remedy a peculiar issue I was having with the seeds. The problem dissapeared without a reason...
+
+// sequelize.sync({ force: true })
+//   .then(async () => {
+//     console.log('\n----- DATABASE SYNCED -----\n');
+//     await seedCategories();
+//   })
+//   .then(async () => {
+//     console.log('\n----- CATEGORIES SEEDED -----\n');
+//     await seedProducts();
+//   })
+//   .then(async () => {
+//     console.log('\n----- PRODUCTS SEEDED -----\n');
+//     await seedTags();
+//   })
+//   .then(async () => {
+//     console.log('\n----- TAGS SEEDED -----\n');
+//     await seedProductTags();
+//   })
+//   .then(() => {
+//     console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+//     process.exit(0);
+//   })
--- seeds/product-tag-seeds.js
+++ seeds/product-tag-seeds.js
@@ -1,22 +1,22 @@
 const { ProductTag } = require('../models');
 
 const productTagData = [
-  {
-    product_id: 1,
-    tag_id: 6,
-  },
-  {
-    product_id: 1,
-    tag_id: 7,
-  },
-  {
-    product_id: 1,
-    tag_id: 8,
-  },
-  {
-    product_id: 2,
-    tag_id: 6,
-  },
+  // {
+  //   product_id: 1,
+  //   tag_id: 6,
+  // },
+  // {
+  //   product_id: 1,
+  //   tag_id: 7,
+  // },
+  // {
+  //   product_id: 1,
+  //   tag_id: 8,
+  // },
+  // {
+  //   product_id: 2,
+  //   tag_id: 6,
+  // },
   {
     product_id: 3,
     tag_id: 1,
@@ -41,14 +41,14 @@ const productTagData = [
     product_id: 4,
     tag_id: 2,
   },
-  {
-    product_id: 4,
-    tag_id: 8,
-  },
+  // {
+  //   product_id: 4,
+  //   tag_id: 8,
+  // },
   {
     product_id: 5,
     tag_id: 3,
-  },
+  }
 ];
 
 const seedProductTags = () => ProductTag.bulkCreate(productTagData);
